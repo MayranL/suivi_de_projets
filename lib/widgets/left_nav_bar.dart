d41b257864caa33ec5e8345442b859396405483b
Jean-Marc DUVAL (7011576d) <jean-marc-duval@sncf.fr>
2022-06-16 14:23:13 +0200

    beginning left navbar creation

---
+++ lib/widgets/left_nav_bar.dart
@@ -0,0 +1,16 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class LeftNavBar extends StatelessWidget {
+  const LeftNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 5,
+      padding: const EdgeInsets.all(10),
+      child: ,
+    );
+  }
+}
